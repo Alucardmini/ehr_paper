1955c290739feb0bb269e7157e26c83a5126dc7f
wuxikun <linuxjade@163.com>
2018-12-21 16:52:49 +0800

    hehe

--- src/deep_learn_process.py
+++ src/deep_learn_process.py
@@ -7,15 +7,27 @@
 @file: deep_learn_process.py
 @time: 12/21/18 3:31 PM
 """
+
 import os
 import sys
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+sys.path.append(os.path.split(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))[0])
+
 import pickle
 from src.datasets import Datesets
 from keras.layers import Embedding, Dense, LSTM
+from keras.preprocessing import sequence
+from keras.models import Model, Sequential
+import numpy as np
+from keras.utils import np_utils
 
 (raw_datas, labels) = Datesets.load_data("in_hospital")
 
-nb_class = max(labels)
+nb_class = max(labels) + 1
+batch_size = 4
 
 train_size = int(len(labels) * 0.8)
 train_x = raw_datas[:train_size]
@@ -47,7 +59,25 @@ def vector_sentence(sentence, wordIndices):
 
 train_x = [[vector_sentence(doc, word_indices) for doc in usr] for usr in train_x]
 test_x = [[vector_sentence(doc, word_indices) for doc in usr] for usr in test_x]
+train_x = [sequence.pad_sequences(_, max_length) for _ in train_x]
+test_x = [sequence.pad_sequences(_, max_length) for _ in test_x]
 
+test_x = np.array(test_x)
+test_y = np.array(test_y)
+train_x = np.array(train_x)
+train_y = np.array(train_y)
+train_y = np_utils.to_categorical(train_y, nb_class)
+test_y = np_utils.to_categorical(test_y, nb_class)
 
+model = Sequential()
+model.add(Embedding(len(word_indices), output_dim=max_length, name='embedding'))
+model.add(LSTM(512))
+model.add(Dense(512))
+model.add(Dense(nb_class, activation='softmax'))
 
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(train_x, train_y, batch_size=batch_size)
 
