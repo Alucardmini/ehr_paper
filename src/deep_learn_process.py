41c518ecbd9719b7d26276869a85251dd7b092f7
wuxikun <linuxjade@163.com>
2018-12-21 17:52:36 +0800

    gpu

--- src/deep_learn_process.py
+++ src/deep_learn_process.py
@@ -22,7 +22,14 @@ from keras.layers import Embedding, Dense, LSTM
 from keras.preprocessing import sequence
 from keras.models import Model, Sequential
 import numpy as np
+import keras.backend as K
 from keras.utils import np_utils
+import tensorflow as tf
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+config = tf.ConfigProto
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
 
 (raw_datas, labels) = Datesets.load_data("in_hospital")
 
