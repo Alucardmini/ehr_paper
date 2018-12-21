d6fc12722dc5942821cd26992dff3bc6f62712c0
wuxikun <linuxjade@163.com>
2018-12-21 13:41:32 +0800

    test

--- src/datasets.py
+++ src/datasets.py
@@ -3,78 +3,88 @@
 
 import json
 
-def get_sample_datesets(data_path, task, sample_nums=1000, is_sampls=False):
-    """
-    以12小时为单位存储
-    :param train_path:训练数据的路径
-    :param task:任务：'mortality/readmission/diagnosis/in_hospital'
-    :param predict_point:'admission/admission_after_24h/discharge'
-    :return: 返回训练集文本和训练标签,记录每个样本有多少个12小时
-    """
-    # 加载训练集
-    train_data = json.load(open(data_path + '/train_data.txt', 'r', encoding='utf-8'))
-    # 加载测试疾病resArr=['desc1','desc1',...]疾病名字
-    y_label = json.load(open(data_path + '/train_label.txt', 'r', encoding='utf-8'))
-    new_y_label = []
-    new_train_data = []
-    sample_count = {}
 
-    total = len(train_data)
-    if is_sampls and sample_nums > 0:
-        total = sample_nums
-    # 取出每一份样本
-    for i in range(total):
-        every_bingli = train_data[i]
-        count = 0
-        if task == 'mortality':
-            # 取出每一个12小时,放入new_train_data中
-            for j in range(len(every_bingli)):
-                count = count + 1
-                new_data = " ".join(every_bingli[j][:-1])
-                new_train_data.append(new_data)
-            # 记录每个样本有多少个12小时
-            sample_count[i] = count
-            # 取出每一个标签
-            new_y_label.append(y_label[i][2])
-        elif task == 'readmission':
-            for j in range(len(every_bingli)):
-                count = count + 1
-                new_data = " ".join(every_bingli[j][:-1])
-                new_train_data.append(new_data)
-            sample_count[i] = count
-            new_y_label.append(y_label[i][0])
-        elif task == 'in_hospital':
-            for j in range(len(every_bingli)):
-                if not y_label[i][3]:
-                    continue
-                count = count + 1
-                new_data = " ".join(every_bingli[j][:-1])
-                new_train_data.append(new_data)
-            sample_count[i] = count
-            new_y_label.append(y_label[i][3])
-        elif task == 'diagnosis':
-            pass
-    max_sample_count = sample_count[max(sample_count)]
-    print(max_sample_count)
-    return new_train_data, new_y_label, sample_count
+class Datesets(object):
 
+    def __init__(self):
+        pass
 
-def write_list_2_file(list, filepath):
-    with open(filepath, 'w+') as f:
-        f.write(str(list))
+    def get_sample_datesets(self, data_path, task, sample_nums=1000, is_sampls=False):
+        """
+        以12小时为单位存储
+        :param train_path:训练数据的路径
+        :param task:任务：'mortality/readmission/diagnosis/in_hospital'
+        :param predict_point:'admission/admission_after_24h/discharge'
+        :return: 返回训练集文本和训练标签,记录每个样本有多少个12小时
+        """
+        # 加载训练集
+        train_data = json.load(open(data_path + '/train_data.txt', 'r', encoding='utf-8'))
+        # 加载测试疾病resArr=['desc1','desc1',...]疾病名字
+        y_label = json.load(open(data_path + '/train_label.txt', 'r', encoding='utf-8'))
+        new_y_label = []
+        new_train_data = []
+        sample_count = {}
 
+        total = len(train_data)
+        if is_sampls and sample_nums > 0:
+            total = sample_nums
+        # 取出每一份样本
+        for i in range(total):
+            every_bingli = train_data[i]
+            count = 0
+            if task == 'mortality':
+                # 取出每一个12小时,放入new_train_data中
+                for j in range(len(every_bingli)):
+                    count = count + 1
+                    new_data = " ".join(every_bingli[j][:-1])
+                    new_train_data.append(new_data)
+                # 记录每个样本有多少个12小时
+                sample_count[i] = count
+                # 取出每一个标签
+                new_y_label.append(y_label[i][2])
+            elif task == 'readmission':
+                for j in range(len(every_bingli)):
+                    count = count + 1
+                    new_data = " ".join(every_bingli[j][:-1])
+                    new_train_data.append(new_data)
+                sample_count[i] = count
+                new_y_label.append(y_label[i][0])
+            elif task == 'in_hospital':
+                for j in range(len(every_bingli)):
+                    if not y_label[i][3]:
+                        continue
+                    count = count + 1
+                    new_data = " ".join(every_bingli[j][:-1])
+                    new_train_data.append(new_data)
+                sample_count[i] = count
+                new_y_label.append(y_label[i][3])
+            elif task == 'diagnosis':
+                pass
+        max_sample_count = sample_count[max(sample_count)]
+        print(max_sample_count)
+        return new_train_data, new_y_label, sample_count
 
-def save_train_data(train_x, train_y, dst_path):
-    write_list_2_file(train_x, dst_path+'_x.txt')
-    write_list_2_file(train_y, dst_path+'_y.txt')
+    def write_list_2_file(self, src_list, filepath):
+        with open(filepath, 'w+') as f:
+            lists = [line + "\n" for line in src_list]
+            f.writelines(lists)
+
+    def save_train_data(self, train_x, train_y, dst_path):
+        self.write_list_2_file(train_x, dst_path+'_x.txt')
+        self.write_list_2_file(train_y, dst_path+'_y.txt')
 
 if __name__ == "__main__":
 
+    data_app = Datesets()
     tasks = ['mortality', 'readmission', 'in_hospital']
     data_path = "/home/jq/PaperRealization/data"
     for task in tasks:
-        texts, labels, sample_count = get_sample_datesets(data_path=data_path, task=task, sample_nums=1000, is_sampls=True)
-        save_train_data(texts, labels, '../data/'+task)
+        texts, labels, sample_count = data_app.get_sample_datesets(data_path=data_path, task=task, sample_nums=1000, is_sampls=True)
+        data_app.save_train_data(texts, labels, '../data/'+task)
 
+    # src_str = open('../data/in_hospital_x.txt', 'r').read()
+    # content = str(src_str)
+    # print(content[len(content)-10:])
+    # # test_json = json.load(content)
 
 
