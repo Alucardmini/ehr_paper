6917885d3203861acbbf1391c7fd6116c30045f4
wuxikun <linuxjade@163.com>
2018-12-21 18:08:03 +0800

    init

---
+++ src/datasets.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import json
+import pickle
+
+class Datesets(object):
+
+    def __init__(self):
+        pass
+
+    def get_sample_datesets(self, data_path, task, sample_nums=1000, is_sampls=False):
+        """
+        以12小时为单位存储
+        :param train_path:训练数据的路径
+        :param task:任务：'mortality/readmission/diagnosis/in_hospital'
+        :param predict_point:'admission/admission_after_24h/discharge'
+        :return: 返回训练集文本和训练标签,记录每个样本有多少个12小时
+        """
+        # 加载训练集
+        train_data = json.load(open(data_path + '/train_data.txt', 'r', encoding='utf-8'))
+        # 加载测试疾病resArr=['desc1','desc1',...]疾病名字
+        y_label = json.load(open(data_path + '/train_label.txt', 'r', encoding='utf-8'))
+        new_y_label = []
+        new_train_data = []
+        sample_count = {}
+
+        total = len(train_data)
+        if is_sampls and sample_nums > 0:
+            total = sample_nums
+        # 取出每一份样本
+        for i in range(total):
+            every_bingli = train_data[i]
+            count = 0
+            if task == 'mortality':
+                # 取出每一个12小时,放入new_train_data中
+                for j in range(len(every_bingli)):
+                    count = count + 1
+                    new_data = " ".join(every_bingli[j][:-1])
+                    new_train_data.append(new_data)
+                # 记录每个样本有多少个12小时
+                sample_count[i] = count
+                # 取出每一个标签
+                new_y_label.append(y_label[i][2])
+            elif task == 'readmission':
+                for j in range(len(every_bingli)):
+                    count = count + 1
+                    new_data = " ".join(every_bingli[j][:-1])
+                    new_train_data.append(new_data)
+                sample_count[i] = count
+                new_y_label.append(y_label[i][0])
+            elif task == 'in_hospital':
+                for j in range(len(every_bingli)):
+                    if not y_label[i][3]:
+                        continue
+                    count = count + 1
+                    new_data = " ".join(every_bingli[j][:-1])
+                    new_train_data.append(new_data)
+                sample_count[i] = count
+                new_y_label.append(y_label[i][3])
+            elif task == 'diagnosis':
+                pass
+        max_sample_count = sample_count[max(sample_count)]
+        print(max_sample_count)
+        return new_train_data, new_y_label, sample_count
+
+    def write_list_2_file(self, src_list, filepath):
+        with open(filepath, 'wb') as f:
+            pickle.dump(src_list, f)
+
+    def save_train_data(self, train_x, train_y, sample_map, dst_path):
+        self.write_list_2_file(train_x, dst_path+'_x.pkl')
+        self.write_list_2_file(train_y, dst_path+'_y.pkl')
+        self.write_list_2_file(sample_map, dst_path+'_map.pkl')
+
+    def load_data(self, task='in_hospital'):
+        """
+
+        :param task:根据任务加载数据  task in ['mortality', 'readmission', 'in_hospital']
+        :return:
+        """
+        with open('../data/'+task+'_x.pkl', 'rb')as f:
+            train_x = pickle.load(f)
+        with open('../data/'+task+'_y.pkl', 'rb')as f:
+            train_y = pickle.load(f)
+        with open('../data/'+task+'_map.pkl', 'rb')as f:
+            train_map = pickle.load(f)
+        train_data = []
+        base = 0
+
+        for k, v in train_map.items():
+            if base + train_map[k] <= len(train_x):
+                train_data.append(' '.join(train_x[base: base+train_map[k]]))
+                # train_data.append(train_x[base: base+train_map[k]])
+                base += train_map[k]
+
+        return (train_data, train_y)
+
+if __name__ == "__main__":
+
+    data_app = Datesets()
+    # tasks = ['mortality', 'readmission', 'in_hospital']
+    # data_path = "/home/jq/PaperRealization/data"
+    # for task in tasks:
+    #     texts, labels, sample_map = data_app.get_sample_datesets(data_path=data_path, task=task, sample_nums=1000, is_sampls=True)
+    #     data_app.save_train_data(texts, labels, sample_map, '../data/'+task)
+
+    # data_app = Datesets()
+    # src_list = ['1', '2', '3']
+    # data_app.write_list_2_file(src_list, '../data/demo.pkl')
+
+    # with open('../data/in_hospital_x.pkl', 'rb')as f:
+    #     train_x = pickle.load(f)
+    #     print(len(train_x))
+    #
+    # with open('../data/in_hospital_y.pkl', 'rb')as f:
+    #     train_y = pickle.load(f)
+    #
+    # with open('../data/in_hospital_map.pkl', 'rb')as f:
+    #     train_map = pickle.load(f)
+    # print(len(train_map))
+    data_app.load_data('in_hospital')
+
+
+
---
+++ src/deep_learn_process.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+#coding:utf-8
+
+"""
+@author: wuxikun
+@software: PyCharm Community Edition
+@file: deep_learn_process.py
+@time: 12/21/18 3:31 PM
+"""
+
+import os
+import sys
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+sys.path.append(os.path.split(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))[0])
+
+import pickle
+from src.datasets import Datesets
+from keras.layers import Embedding, Dense, LSTM
+from keras.preprocessing import sequence
+from keras.models import Model, Sequential
+import numpy as np
+import keras.backend as K
+from keras.utils import np_utils
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # 使用编号为1，2号的GPU
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 每个GPU现存上界控制在50%以内
+session = tf.Session(config=config)
+
+KTF.set_session(session)
+
+
+(raw_datas, labels) = Datesets.load_data("in_hospital")
+
+nb_class = max(labels) + 1
+batch_size = 4
+
+train_size = int(len(labels) * 0.8)
+train_x = raw_datas[:train_size]
+train_y = labels[:train_size]
+test_x = raw_datas[train_size:]
+test_y = labels[train_size:]
+
+w2id_path = '../data/word2id.pkl'
+
+# without flatten
+# max_length = max([len(doc.split(' ')) for usr in train_x for doc in usr])
+# max_steps = max([len(_) for _ in train_x])   #　文书最多个数
+
+
+max_length = max([len(usr.split(' ')) for usr in train_x])
+# max_steps = max([len(_) for _ in train_x])   #　文书最多个数
+
+
+if not os.path.exists(w2id_path):
+    words = set()
+    for docs in train_x:
+        for record in docs:
+            record_list = record.split(' ')
+            words = (words | set(record_list))
+    word_indices = {v: i for i, v in enumerate(words)}
+    pickle.dump(word_indices, open(w2id_path, 'wb+'))
+else:
+    word_indices = pickle.load(open(w2id_path, 'rb'))
+
+
+def vector_sentence(sentence, wordIndices):
+    sent_list = sentence.strip().split(' ')
+    res = [word_indices[v] for v in sent_list if v in wordIndices]
+    return res
+
+# train_x = [[vector_sentence(doc, word_indices) for doc in usr] for usr in train_x]
+# test_x = [[vector_sentence(doc, word_indices) for doc in usr] for usr in test_x]
+# train_x = [sequence.pad_sequences(_, max_length) for _ in train_x]
+# test_x = [sequence.pad_sequences(_, max_length) for _ in test_x]
+
+train_x = [vector_sentence(doc, word_indices) for doc in train_x]
+test_x = [vector_sentence(doc, word_indices) for doc in test_x]
+train_x = sequence.pad_sequences(train_x, max_length)
+test_x = sequence.pad_sequences(test_x, max_length)
+
+test_x = np.array(test_x)
+test_y = np.array(test_y)
+train_x = np.array(train_x)
+train_y = np.array(train_y)
+train_y = np_utils.to_categorical(train_y, nb_class)
+test_y = np_utils.to_categorical(test_y, nb_class)
+
+print(train_x[0].shape)
+
+model = Sequential()
+model.add(Embedding(len(word_indices), output_dim=64, name='embedding'))
+model.add(LSTM(512))
+model.add(Dense(512))
+model.add(Dense(nb_class, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(train_x, train_y, batch_size=batch_size, epochs=40)
+model.save('in_hospital.h5')
