c33495d4e32caeb80804524c84d63e3c1d3670a3
wuxikun <linuxjade@163.com>
2018-12-24 14:13:52 +0800

    xx

--- src/deep_learn_process.py
+++ src/deep_learn_process.py
@@ -26,6 +26,7 @@ import keras.backend as K
 from keras.utils import np_utils
 import tensorflow as tf
 import keras.backend.tensorflow_backend as KTF
+from keras import regularizers, optimizers
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # 使用编号为1，2号的GPU
 config = tf.ConfigProto()
@@ -34,17 +35,9 @@ session = tf.Session(config=config)
 
 KTF.set_session(session)
 
-# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"]="0"
-# config = tf.ConfigProto
-# config.gpu_options.per_process_gpu_memory_fraction = 0.5
-
-
 (raw_datas, labels) = Datesets.load_data("in_hospital")
-
 raw_datas = [list(set(data.split(' '))) for data in raw_datas]
 
-nb_class = max(labels) + 1
 batch_size = 32
 epochs = 50
 
@@ -55,12 +48,6 @@ test_x = raw_datas[train_size:]
 test_y = labels[train_size:]
 
 w2id_path = '../data/word2id.pkl'
-
-# without flatten
-# max_length = max([len(doc.split(' ')) for usr in train_x for doc in usr])
-# max_steps = max([len(_) for _ in train_x])   #　文书最多个数
-
-
 max_length = max([len(usr) for usr in train_x])
 # max_steps = max([len(_) for _ in train_x])   #　文书最多个数
 
@@ -85,11 +72,6 @@ def vector_sentence(sentence, wordIndices):
     sent_list = sentence.strip().split(' ')
     return vecotr_list(sent_list, wordIndices)
 
-# train_x = [[vector_sentence(doc, word_indices) for doc in usr] for usr in train_x]
-# test_x = [[vector_sentence(doc, word_indices) for doc in usr] for usr in test_x]
-# train_x = [sequence.pad_sequences(_, max_length) for _ in train_x]
-# test_x = [sequence.pad_sequences(_, max_length) for _ in test_x]
-
 train_x = [vecotr_list(doc, word_indices) for doc in train_x]
 test_x = [vecotr_list(doc, word_indices) for doc in test_x]
 train_x = sequence.pad_sequences(train_x, max_length)
@@ -99,21 +81,22 @@ test_x = np.array(test_x)
 test_y = np.array(test_y)
 train_x = np.array(train_x)
 train_y = np.array(train_y)
-# train_y = np_utils.to_categorical(train_y, nb_class)
-# test_y = np_utils.to_categorical(test_y, nb_class)
 
 print(train_x[0].shape)
 
 model = Sequential()
-model.add(Embedding(len(word_indices), output_dim=64, name='embedding'))
-model.add(Dropout(0.441))
+model.add(Embedding(len(word_indices), output_dim=128, name='embedding', dropout=0.4410))
 model.add(Dense(100))
 model.add(LSTM(279))
 model.add(Dense(20))
 model.add(Dense(1, activation='linear'))
 
+
+sgd = optimizers.SGD(lr=0.01, clipnorm=11.16)
+
+
 model.compile(loss='mean_squared_error',
-              optimizer='SGD',
+              optimizer=sgd,
               metrics=['accuracy'])
 
 model.fit(train_x, train_y, batch_size=batch_size, epochs=epochs, validation_data=(test_x, test_y))
